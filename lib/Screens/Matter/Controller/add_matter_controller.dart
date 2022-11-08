6c0d6ed0dc266579182d78a6f7c27e4b4bc9e824
diego daniel <diegodanielsantana00@gmail.com>
2022-11-08 09:55:04 -0300

    08-11-2022 --> Validation save matter finish with awesome dialog

--- lib/Screens/Matter/Controller/add_matter_controller.dart
+++ lib/Screens/Matter/Controller/add_matter_controller.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: non_constant_identifier_names, use_build_context_synchronously, unused_local_variable
+// ignore_for_file: non_constant_identifier_names, use_build_context_synchronously, unused_local_variable, avoid_single_cascade_in_expression_statements, prefer_const_constructors
 
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:vexa_flashcards/Common/Navigator.dart';
 import 'package:vexa_flashcards/Common/SQLiteHelper.dart';
@@ -9,13 +10,53 @@ import 'package:vexa_flashcards/Screens/Matter/Widget/add_matter_widgets.dart';
 class AddMatterController {
   Function() SaveMatter(BuildContext context, AddMatterWigdet addMatterWigdet) {
     return () async {
-      Matter matterAux = Matter(memory: 0, title: addMatterWigdet.titleEditingController.text, color: addMatterWigdet.selectColorOption.toString());
-      int id_matter = await DatabaseHelper().insertDatabase("matter", matterAux);
-      for (var element in addMatterWigdet.listCards) {
-        element.id_matter = id_matter;
-        await DatabaseHelper().insertDatabase("card", element);
+      if (addMatterWigdet.titleEditingController.text.isEmpty) {
+        AwesomeDialog(
+          context: context,
+          animType: AnimType.scale,
+          dialogType: DialogType.error,
+          body: Center(
+            child: Text(
+              "Adicione um título para sua matéria",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+          // title: 'This is Ignored',
+          autoHide: Duration(seconds: 4),
+          headerAnimationLoop: false,
+          // desc: 'This is also Ignored',
+          btnCancelOnPress: (){},
+          btnCancelText: "OK"
+        )..show();
+      } else if (addMatterWigdet.listCards.length <= 2) {
+        AwesomeDialog(
+          context: context,
+          animType: AnimType.scale,
+          dialogType: DialogType.error,
+          body: Center(
+            child: Text(
+              "Adicione pelo menos três carta na sua nova matéria ${addMatterWigdet.titleEditingController.text}",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+          // title: 'This is Ignored',
+          autoHide: Duration(seconds: 4),
+          headerAnimationLoop: false,
+          // desc: 'This is also Ignored',
+          btnCancelOnPress: (){},
+          btnCancelText: "OK"
+        )..show();
+      } else {
+        Matter matterAux = Matter(memory: 0, title: addMatterWigdet.titleEditingController.text, color: addMatterWigdet.selectColorOption.toString());
+        int id_matter = await DatabaseHelper().insertDatabase("matter", matterAux);
+        for (var element in addMatterWigdet.listCards) {
+          element.id_matter = id_matter;
+          await DatabaseHelper().insertDatabase("card", element);
+        }
+        NavigatorController().navigatorBack(context);
       }
-      NavigatorController().navigatorBack(context);
     };
   }
 }
